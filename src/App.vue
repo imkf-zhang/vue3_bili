ef608e206e8463d2c09778736433f08556c33de5
zhangkaifan <15638452918@163.com>
2024-08-08 22:56:09 +0800

    feat: 使用命令式实现一个计数器

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header v-for="">
+  <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// import { ref } from '@vue/reactivity'
 createApp(App).mount('#app')
-
-const name = 'vue'
-// name = 123
+// console.log('ref', ref)
