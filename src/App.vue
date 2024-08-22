5531de9db74ba8a1f82f497369073c96b1bdd79c
zhangkaifan <15638452918@163.com>
2024-08-22 23:12:52 +0800

    feat: 代办功能完成

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>hi {{msg}}</h1>
+    <!-- <div @click="add"> {{count}}</div> -->
+    <input
+      type="text"
+      v-model="val"
+    >
+    <button @click="addToDO">add</button>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo.title"
+      >
+        <input
+          type="checkbox"
+          v-model="todo.done"
+        >
+        <span>{{todo.title}}</span>
+      </li>
+    </ul>
+    <div>{{doneCount+ '/' + totalTodo}}</div>
+    <input
+      type="checkbox"
+      v-model="allDone"
+    >
+    <!-- <div @click="addToDO">addToDO</div> -->
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data () {
+    return {
+      msg: "vue js",
+      count: 1,
+      val: "",
+      todos: [
+        {title: "吃饭", done: true},
+        {title: "睡觉", done: true}
+        ]
+    }
+  },
+  computed: {
+    doneCount() {
+      return this.todos.filter(item => item.done).length
+    },
+    totalTodo() {
+      return this.todos.length
+    },
+    allDone: {
+      get() {
+       return this.doneCount === this.todos.length
+      },
+      set(val) {
+        this.todos.forEach(item => item.done = val)
+      }
+    }
+  },
+  methods: {
+    add () {
+      this.count++
+    },
+    addToDO() {
+      if(!this.val) {
+        alert('必须填内容')
+        return
+      }
+      this.todos.push({title:this.val,done:false})
+      this.val = ""
+    }
+  },
+})
+</script>
+<style>
 </style>
