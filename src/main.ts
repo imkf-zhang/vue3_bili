8191cae5ee3b4b2f3109c12ad3b80456e0776b31
zhangkaifan <15638452918@163.com>
2025-02-21 22:56:32 +0800

    fix：试一下 npm run lint命令

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,8 @@ import App from './App.vue'
 // import { ref } from '@vue/reactivity'
 createApp(App).mount('#app')
 // console.log('ref', ref)
+
+
+// npm run lint 命令  
+// const name = "vue3"
+//   warning  'name' is assigned a value but never used  @typescript-eslint/no-unused-vars
