530265df2f3277f984cca383408f0c5107dde689
zhangkaifan <15638452918@163.com>
2025-02-21 23:03:19 +0800

    test: husky 是否生效

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,9 @@
 <template>
-  <div>
-    <h1>hi {{msg}}</h1>
-    <!-- <div @click="add"> {{count}}</div> -->
-    <input
-      type="text"
-      v-model="val"
-    >
-    <button @click="addToDO">add</button>
-    <ul>
-      <li
-        v-for="todo in todos"
-        :key="todo.title"
-      >
-        <input
-          type="checkbox"
-          v-model="todo.done"
-        >
-        <span>{{todo.title}}</span>
-      </li>
-    </ul>
-    <div>{{doneCount+ '/' + totalTodo}}</div>
-    <input
-      type="checkbox"
-      v-model="allDone"
-    >
-    <!-- <div @click="addToDO">addToDO</div> -->
+  <div v-for="">
+
   </div>
 </template>
-<script>
-import { defineComponent } from 'vue'
-export default defineComponent({
-  data () {
-    return {
-      msg: "vue js",
-      count: 1,
-      val: "",
-      todos: [
-        {title: "吃饭", done: true},
-        {title: "睡觉", done: true}
-        ]
-    }
-  },
-  computed: {
-    doneCount() {
-      console.log('set时进行了触发1')
-      return this.todos.filter(item => item.done).length
-    },
-    totalTodo() {
-      return this.todos.length
-    },
-    allDone: {
-      get() {
-      console.log("set时进行了触发")
-       return this.doneCount === this.todos.length
-      },
-      set(val) {
-        this.todos.forEach(item => item.done = val)
-      }
-    }
-  },
-  methods: {
-    add () {
-      this.count++
-    },
-    addToDO() {
-      if(!this.val) {
-        alert('必须填内容')
-        return
-      }
-      this.todos.push({title:this.val,done:false})
-      this.val = ""
-    }
-  },
-})
+<script setup>
+//  <todo-list> </todo-list>
+// import TodoList from './components/TodoList.vue'
 </script>
-<style>
-</style>
