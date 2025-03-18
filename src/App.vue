cd65f5f75329d931b0816ec9dbbd214c49052351
zhangkaifan <15638452918@163.com>
2025-03-18 21:54:53 +0800

    feat: 父子组件传参

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <rate-components :value="3"></rate-components>
-    <rate-components :value="1+1"></rate-components>
+    <rate-components :value="3" @update="update">
+    书的评分
+    </rate-components>
+    <rate-components :value="1+1">
+    课程评分
+    </rate-components>
     <todo-list :title="title1" itemKey="tolist1"> </todo-list>
     <todo-list :title="title2" itemKey="tolist2"> </todo-list>
     <todo-list itemKey="tolist3"> </todo-list>
@@ -15,4 +19,7 @@ import RateComponents from './components/RateComponents.vue'
 import {ref} from 'vue';
 let title1 = ref('kate的代办清单')
 let title2 = ref('凯帆的代办清单')
+function update(res) {
+  console.log(res)
+}
 </script>
--- src/components/RateComponents.vue
+++ src/components/RateComponents.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    <span>{{score}}</span>
+    <slot></slot>
+    <span @mousemove="tryUpdate">{{score}}</span>
   </div>
 </template>
 <script>
 import { defineComponent} from 'vue'
 // ★☆
 export default defineComponent({
+  // 1、组件内部的渲染和逻辑
+  // 2、组件的输入
+  // 3、组件的输出
+  // 4、组件的扩展
   props: {
     value: {
       type: Number,
       required: true
     }
   },
+  emits: ['update'],
   computed: {
     score() {
       return '★★★★★☆☆☆☆☆'.slice(5-this.value, 10-this.value)
+    }
+  },
+  methods: {
+    tryUpdate() {
+      this.$emit("update", 'hello')
+      console.log("Dayin")
     }
   }
 })
