7cf9196cf6f7bf317fd3d9529e50faf98463eaa4
zhangkaifan <15638452918@163.com>
2025-03-19 22:52:29 +0800

    feat: 引用图片的两种方式 引入sass

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <img :src="logo" alt="">
+    <div v-html="logo1"></div>
+    zkf
     <rate-components :value="3" @update="update">
     书的评分
     </rate-components>
@@ -10,12 +13,16 @@
     <todo-list :title="title2" itemKey="tolist2"> </todo-list>
     <todo-list itemKey="tolist3"> </todo-list>
     <div></div>
-    <h1></h1>
+    <h1>一帆风顺</h1>
   </div>
 </template>
 <script setup>
 import TodoList from './components/TodoList.vue'
 import RateComponents from './components/RateComponents.vue'
+import logo from './assets/logo.svg'
+import logo1 from './assets/logo.svg?raw'
+console.log('logo', logo)
+console.log('logo1', logo1)
 import {ref} from 'vue';
 let title1 = ref('kate的代办清单')
 let title2 = ref('凯帆的代办清单')
@@ -23,3 +30,11 @@ function update(res) {
   console.log(res)
 }
 </script>
+<style lang="scss">
+$primary-color: red;
+$font-size: 20px;
+h1 {
+  color: $primary-color;
+  font-size: $font-size;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./main.css"
 // import { ref } from '@vue/reactivity'
 createApp(App).mount('#app')
 // console.log('ref', ref)
