7c4ed9d33d00939ba36bb0ba470d5d06220ea57f
zhangkaifan <15638452918@163.com>
2025-04-25 21:31:31 +0800

    feat: vue结合ts的一些使用

--- src/components/LearnTs.vue
+++ src/components/LearnTs.vue
@@ -1,31 +1,43 @@
 <template>
-  
+
   <div>
     {{count}}
+    <input
+      ref="input"
+      type="text"
+      v-model="val"
+    >
     <button @click="add">click</button>
-   <div v-for="(item,index) in course" :key="index">
-  {{item.name}}
-  {{item.price}}
-  </div>
+    <div
+      v-for="(item,index) in course"
+      :key="index"
+    >
+      {{item.name}}
+      {{item.price}}
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue';
+import { ref, onMounted, reactive, computed } from 'vue'
 
 let count = ref<number>(1)
+let val = ref('')
+let input = ref<HTMLInputElement | null>(null)
 
 interface Course {
-  name: string,
+  name: string
   price: number
 }
-let course = ref<Course[]>([{name: 'z', price: 99}])
+let course = ref<Course[]>([{ name: 'z', price: 99 }])
 function add() {
   course.value.push({
     name: `vue ${count.value}`,
     price: count.value
   })
-  
 }
+onMounted(() => {
+  input.value?.focus()
+})
 </script>
 <style lang="scss">
 </style>
