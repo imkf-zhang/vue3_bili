5ebdcbe92a7e090968ccbce538fe8113b71cca4e
zhangkaifan <15638452918@163.com>
2026-04-23 23:58:29 +0800

    feat: "删除所有的包重新下载"

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     首页
+    <a-button
+      type="primary"
+      loading
+    >Loading</a-button>
+    <a-date-picker />
   </div>
 </template>
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     legacy(),
-    vue(),
+    vue()
+    // Components({
+    //   resolvers: [
+    //     AntDesignVueResolver({
+    //       importStyle: 'css', // 或者 'less'
+    //       resolveIcons: true,
+    //     })
+    //   ],
+    //   dts: 'src/components.d.ts' 
+    // })
   ],
   resolve: {
     alias: {
